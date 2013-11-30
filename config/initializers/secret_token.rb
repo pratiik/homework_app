9b6b5082d97f5fd119acd17dd410225087048fc4
Pratiik Atrii <prateek.attree@addvalsolutions.com>
2013-11-30 19:00:53 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeworkApp::Application.config.secret_token = 'b181d59254b755e4d3326209597e390c62415fd48f1716021b29d0e9faee33db0f3732fbc9467cc16eaa788d419769ba5f2ca231ece917acbc3ee54e0d4ab77c'
